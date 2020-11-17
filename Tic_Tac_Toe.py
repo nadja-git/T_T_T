d7d6b83a1c6a826af9d3824eb9efe4965f518b43
SANIKS Nadejda <nadejda.saniks@altran.com>
2020-11-17 13:37:50 +0100

    added messages

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,54 +1,26 @@
 # T_T_T
-#Practical repo for "Tic Tac Toe" 
-# wanted to create i Phyton file
+# this is function to check if board is full
 def full_board_check(board): 
-
     count = 0 
-
     for i in range (1,10): 
-
         if board[i] == ' ': 
-
             count += 1 
-
-     
-
     if count == 0: 
-
         return True 
-
     else: 
-
         return False 
-
- 
-
 def player_choice(board): 
-
-     
-
     ref = False 
-
     while ref == False: 
-
         pos = int(input("Choose your cell (between 1-9): ")) 
-
         if pos<1 and pos>9: 
-
             print('Number out of range') 
-
             continue 
-
         if space_check(board, pos): 
-
             ref = True 
-
             return pos 
-
         else: 
-
             print('The cell is already filled! Choose another one') 
-
 # Function to replay the game 
 
 def replay(): 
@@ -56,3 +28,5 @@ def replay():
     reply = input("Do you want to play again? (Y or N): ").upper() 
 
     return reply == 'Y' 
+#added a new comment
+#Now in Nadja_Branch
