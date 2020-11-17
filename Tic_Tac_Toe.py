ba2e2da48075ac733f316a898ea12bec55fba3c7
SAEED Mohammed <mohammed.saeed@altran.com>
2020-11-17 10:29:36 +0100

    Added random function/Moe

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,3 +1,9 @@
 # T_T_T
 Practical repo for "Tic Tac Toe" 
 # wanted to create i Phyton file
+import random
+def choose_first():
+    if random.randint(1,2) == 1:
+        return 'Player 1'
+    else:
+        return 'Player 2'
