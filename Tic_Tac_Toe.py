802a85583debe664005a15a872349e41de6a71bc
MARTINEZ Miguel <miguel.martinez@altran.com>
2020-11-17 10:41:34 +0100

    Miguel added code

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,3 +1,20 @@
 # T_T_T
 Practical repo for "Tic Tac Toe" 
 # wanted to create i Phyton file
+from IPython.display import clear_output 
+
+ 
+
+def display_board(board): 
+
+    clear_output() 
+
+    print(' '+ board[7]+ ' | ' +board[8]+ ' | ' +board[9]) 
+
+    print('-----------') 
+
+    print(' '+ board[4]+ ' | ' +board[5]+ ' | ' +board[6]) 
+
+    print('-----------') 
+
+    print(' '+ board[1]+ ' | ' +board[2]+ ' | ' +board[3]) 
